9ed22b8bd5373c0736c32be438a5cf98c0a8df45
Mahmud Ahmed <freelancermahmud826@gmail.com>
2023-11-23 02:31:26 +0600

    Create The initial repository for JoulesLabs Front-end Devloper job task

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="full-page-wrapper">
+        
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/PdfFile.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PdfFile() {
+  return (
+    <div>PdfFile</div>
+  )
+}
+
+export default PdfFile
